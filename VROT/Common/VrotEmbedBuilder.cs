d95171a0603b29a5ca72ab415812272fbf8c4957
Yaroslav Kovalov <yarik19022001@outlook.com>
2022-07-22 20:51:40 +0300

    Creat info user and common

---
+++ VROT/Common/VrotEmbedBuilder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+
+namespace VROT.Common
+{
+    internal class VrotEmbedBuilder : EmbedBuilder
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WatermelonEmbedBuilder"/> class.
+        /// </summary>
+        public VrotEmbedBuilder()
+        {
+            WithColor(new Color(238, 62, 75));
+        }
+    }
+}
--- VROT/Modules/General.cs
+++ VROT/Modules/General.cs
@@ -1,19 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Discord;
 using Discord.Commands;
+using System.Threading.Tasks;
 using Discord.WebSocket;
+using VROT.Common;
 
 namespace VROT.Modules
 {
     public class General : ModuleBase<SocketCommandContext>
     {
         [Command("ping")]
+        [Alias("p")]
         public async Task PingAsync()
         {
-            await ReplyAsync("Вставай заебал");
+            await Context.Message.ReplyAsync($"Вставай заебал");
         }
+
+        [Command("say")]
+        public async Task EchoAsync([Remainder] string text)
+        {
+            await Context.Channel.SendMessageAsync(text);
+        }
+
+        [Command("info")]
+        public async Task InfoAsync(SocketGuildUser socketGuildUser = null)
+        {
+            if (socketGuildUser == null)
+            {
+                socketGuildUser = Context.User as SocketGuildUser;
+            }
+
+            var embed = new VrotEmbedBuilder()
+                .WithTitle($"{socketGuildUser.Username}#{socketGuildUser.Discriminator}")
+                .AddField("ID", socketGuildUser.Id, true)
+                .AddField("Name", $"{socketGuildUser.Username}#{socketGuildUser.Discriminator}", true)
+                .AddField("Created at", socketGuildUser.CreatedAt, true)
+                .WithThumbnailUrl(socketGuildUser.GetAvatarUrl() ?? socketGuildUser.GetDefaultAvatarUrl())
+                .WithCurrentTimestamp()
+                .Build();
+
+            await ReplyAsync(embed: embed);
+        }
+
     }
 }
