d80ce0d2359f6ed06a0cb7164b78a760b99e1a60
Yaroslav Kovalov <yarik19022001@outlook.com>
2022-07-22 11:14:01 +0300

    add config

--- VROT/Program.cs
+++ VROT/Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Hosting;
+
+
+
+namespace VROT
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            var builder = new HostBuilder()
+                .ConfigureAppConfiguration(x =>
+                {
+                    var config = new ConfigurationBuilder()
+                        .SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("appsetting.json", false, true)
+                        .Build();
+
+                    x.AddConfiguration(config);
+                })
+
+        }
+    }
+}
+
