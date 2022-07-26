a2e987ba05322f362cdbdafb8018582de8434729
Yaroslav Kovalov <yarik19022001@outlook.com>
2022-07-26 15:26:19 +0300

    Test

--- VROT/Modules/General.cs
+++ VROT/Modules/General.cs
@@ -41,7 +41,7 @@ namespace VROT.Modules
                     .Build();
 
                 await ReplyAsync(embed: embed);
-                await Context.Channel.DeleteMessageAsync(Context.Message.Id);
+                //await Context.Channel.DeleteMessageAsync(Context.Message.Id);
             }
         }
 
