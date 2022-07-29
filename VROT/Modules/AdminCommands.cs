38fb5d592065a15200703c929ba352b2b8f98336
Yaroslav Kovalov <yarik19022001@outlook.com>
2022-07-29 19:41:44 +0300

    Created admin command

---
+++ VROT/Modules/AdminCommands.cs
@@ -0,0 +1,52 @@
+﻿using Discord;
+using Discord.Commands;
+using System.Threading.Tasks;
+using Discord.WebSocket;
+using VROT.Common;
+using VROT.Models;
+
+namespace VROT.Modules
+{
+    public class AdminCommands : ModuleBase<SocketCommandContext>
+    {
+        [Command("say")]
+        [RequireUserPermission(GuildPermission.Administrator, ErrorMessage = "У вас нет прав админа, идите нахуй)0))")]
+        public async Task EchoAsync([Remainder] string text)
+        {
+            await Context.Channel.SendMessageAsync(text);
+            await Context.Channel.DeleteMessageAsync(Context.Message.Id);
+        }
+
+        [Command("clear")]
+        [RequireUserPermission(GuildPermission.Administrator, ErrorMessage = "У вас нет прав админа, идите нахуй)0))")]
+        public async Task Clear(int amount)
+        {
+            IEnumerable<IMessage> messages = await Context.Channel.GetMessagesAsync(amount + 1).FlattenAsync();
+            await ((ITextChannel)Context.Channel).DeleteMessagesAsync(messages);
+            const int delay = 3000;
+            IUserMessage m = await ReplyAsync($"I have deleted {amount} messages for ya. :)");
+            await Task.Delay(delay);
+            await m.DeleteAsync();
+        }
+
+        [Command("ban")]
+        [RequireUserPermission(GuildPermission.BanMembers, ErrorMessage = "У вас нет прав банить участников")]
+        public async Task BanMember(SocketGuildUser user = null, [Remainder] string reason = null)
+        {
+            if (user == null)
+            {
+                await ReplyAsync("Пользователь не указан");
+                return;
+            }
+            if (reason == null)
+                reason = "Причина не указана";
+
+            var embed = new VrotEmbedBuilder()
+                .WithDescription($":white_check_mark: {user.Mention}получил банан\n**Причина :** {reason}")
+                .Build();
+
+            await ReplyAsync(embed: embed);
+            await Context.Guild.AddBanAsync(user, 0, reason);
+        }
+    }
+}
