4fb33b170f5e964ef7735c372297aaa133921b07
Yaroslav Kovalov <yarik19022001@outlook.com>
2022-08-10 18:38:48 +0300

    Added FunCommands.cs

---
+++ VROT/Modules/FunCommands.cs
@@ -0,0 +1,47 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using VROT.Common;
+using VROT.Models;
+
+namespace VROT.Modules
+{
+    public class FunCommands : ModuleBase<SocketCommandContext>
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public FunCommands(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [Command("hug")]
+        public async Task Hug()
+        {
+            var httpClient = _httpClientFactory.CreateClient();
+            var response = await httpClient.GetStringAsync("https://miss.perssbest.repl.co/api/v2/hug");
+            var hug = Event.FromJson(response);
+
+            await ReplyAsync(hug.Image);
+        }
+
+        [Command("kiss")]
+        public async Task Kiss()
+        {
+            var httpClient = _httpClientFactory.CreateClient();
+            var response = await httpClient.GetStringAsync("https://miss.perssbest.repl.co/api/v2/kiss");
+            var kiss = Event.FromJson(response);
+
+            await ReplyAsync(kiss.Image);
+        }
+
+        [Command("kill")]
+        public async Task Kill()
+        {
+            var httpClient = _httpClientFactory.CreateClient();
+            var response = await httpClient.GetStringAsync("https://miss.perssbest.repl.co/api/v2/kill");
+            var kill = Event.FromJson(response);
+
+            await ReplyAsync(kill.Image);
+        }
+    }
+}
