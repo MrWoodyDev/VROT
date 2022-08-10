5cd810e4c64ea4e05c469a8e5a68de2a456e5401
Yaroslav Kovalov <yarik19022001@outlook.com>
2022-08-10 20:03:35 +0300

    Refactoring command

--- VROT/Modules/FunCommands.cs
+++ VROT/Modules/FunCommands.cs
@@ -1,4 +1,5 @@
 ﻿using Discord.Commands;
+using NekosSharp;
 using VROT.Models;
 
 namespace VROT.Modules
@@ -11,26 +12,6 @@ namespace VROT.Modules
             _httpClientFactory = httpClientFactory;
         }
 
-        [Command("hug")]
-        public async Task Hug()
-        {
-            var httpClient = _httpClientFactory.CreateClient();
-            var response = await httpClient.GetStringAsync("https://miss.perssbest.repl.co/api/v2/hug");
-            var hug = Event.FromJson(response);
-
-            await ReplyAsync(hug.Image);
-        }
-
-        [Command("kiss")]
-        public async Task Kiss()
-        {
-            var httpClient = _httpClientFactory.CreateClient();
-            var response = await httpClient.GetStringAsync("https://miss.perssbest.repl.co/api/v2/kiss");
-            var kiss = Event.FromJson(response);
-
-            await ReplyAsync(kiss.Image);
-        }
-
         [Command("kill")]
         public async Task Kill()
         {
@@ -39,6 +20,58 @@ namespace VROT.Modules
             var kill = Event.FromJson(response);
 
             await ReplyAsync(kill.Image);
+        }
+
+        public static NekosSharp.NekoClient NekoClient = new NekoClient("VROT");
+
+        [Command("slap")]
+        public async Task Slap()
+        {
+            Request requesteq = await NekoClient.Action_v3.SlapGif();
+            await ReplyAsync(requesteq.ImageUrl);
+
+        }
+
+        [Command("kiss")]
+        public async Task Kiss()
+        {
+            Request request = await NekoClient.Action_v3.KissGif();
+            await ReplyAsync(request.ImageUrl);
+        }
+
+        [Command("feed")]
+        public async Task Feed()
+        {
+            Request request = await NekoClient.Action_v3.FeedGif();
+            await ReplyAsync(request.ImageUrl);
+        }
+
+        [Command("neko")]
+        public async Task Neko()
+        {
+            Request request = await NekoClient.Image_v3.NekoGif();
+            await ReplyAsync(request.ImageUrl);
+        }
+
+        [Command("fox")]
+        public async Task Fox()
+        {
+            Request request = await NekoClient.Image_v3.Fox();
+            await ReplyAsync(request.ImageUrl);
+        }
+
+        [Command("hug")]
+        public async Task Hug()
+        {
+            Request request = await NekoClient.Action_v3.HugGif();
+            await ReplyAsync(request.ImageUrl);
+        }
+
+        [Command("poke")]
+        public async Task Poke()
+        {
+            Request request = await NekoClient.Action_v3.PokeGif();
+            await ReplyAsync(request.ImageUrl);
         }
     }
 }
