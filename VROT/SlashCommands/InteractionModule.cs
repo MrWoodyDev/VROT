fec6a2b32655ce9c95829c9571248cd52a6d9e48
Yaroslav Kovalov <yarik19022001@outlook.com>
2022-08-17 22:37:32 +0300

    Added slash command purge

---
+++ VROT/SlashCommands/InteractionModule.cs
@@ -0,0 +1,13 @@
+﻿using Discord.Interactions;
+
+namespace VROT.SlashCommands
+{
+    public class InteractionModule : InteractionModuleBase<SocketInteractionContext>
+    {
+        [SlashCommand("echo", "Echo an input")]
+        public async Task Echo(string input)
+        {
+            await RespondAsync(input);
+        }
+    }
+}
