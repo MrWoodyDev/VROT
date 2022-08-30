7a30c278274243a93167b3f3fc5684dd56a7abd6
Yaroslav Kovalov <yarik19022001@outlook.com>
2022-08-30 18:46:27 +0300

    upd commands

--- VROT/Modules/FunCommands.cs
+++ VROT/Modules/FunCommands.cs
@@ -1,69 +1,99 @@
 ﻿using Discord.Commands;
 using Discord.WebSocket;
 using VROT.Common;
+using VROT.Services;
 
 namespace VROT.Modules
 {
     public class FunCommands : ModuleBase<SocketCommandContext>
     {
-        [Command("kill")]
-        public async Task Kill(SocketGuildUser user = null!, EmbedCommand embedCommand = null!)
+        private readonly ITenorService _tenorService;
+
+        public FunCommands(ITenorService tenorService)
         {
-            await embedCommand.RunGifCommandAsync(user, $"**{Context.Message.Author.Username}** убил **{user.Username}**", "anime kill");
+            _tenorService = tenorService;
+        }
+
+        [Command("kill")]
+        public async Task Kill(SocketGuildUser user = null!)
+        {
+            await RunGifCommandAsync(user, $"**{Context.Message.Author.Username}** убил **{user.Username}**", "anime kill");
         }
 
         [Command("slap")]
-        public async Task Slap(SocketGuildUser user = null!, EmbedCommand embedCommand = null!)
+        public async Task Slap(SocketGuildUser user = null!)
         {
-            await embedCommand.RunGifCommandAsync(user, $"**{Context.Message.Author.Username}** дал пощёчину **{user.Username}**", "anime slap");
+            await RunGifCommandAsync(user, $"**{Context.Message.Author.Username}** дал пощёчину **{user.Username}**", "anime slap");
         }
 
         [Command("punch")]
-        public async Task Punch(SocketGuildUser user = null!, EmbedCommand embedCommand = null!)
+        public async Task Punch(SocketGuildUser user = null!)
         {
-            await embedCommand.RunGifCommandAsync(user, $"**{Context.Message.Author.Username}** дал пизды **{user.Username}**", "anime punch");
+            await RunGifCommandAsync(user, $"**{Context.Message.Author.Username}** дал пизды **{user.Username}**", "anime punch");
         }
 
         [Command("kiss")]
-        public async Task Kiss(SocketGuildUser user = null!, EmbedCommand embedCommand = null!)
+        public async Task Kiss(SocketGuildUser user = null!)
         {
-            await embedCommand.RunGifCommandAsync(user, $"**{Context.Message.Author.Username}** поцеловал **{user.Username}**", "anime kiss");
+            await RunGifCommandAsync(user, $"**{Context.Message.Author.Username}** поцеловал **{user.Username}**", "anime kiss");
         }
 
         [Command("feed")]
-        public async Task Feed(SocketGuildUser user = null!, EmbedCommand embedCommand = null!)
+        public async Task Feed(SocketGuildUser user = null!)
         {
-            await embedCommand.RunGifCommandAsync(user, $"**{Context.Message.Author.Username}** покормил **{user.Username}**", "anime feed");
+            await RunGifCommandAsync(user, $"**{Context.Message.Author.Username}** покормил **{user.Username}**", "anime feed");
         }
 
         [Command("hug")]
-        public async Task Hug(SocketGuildUser user = null!, EmbedCommand embedCommand = null!)
+        public async Task Hug(SocketGuildUser user = null!)
         {
-            await embedCommand.RunGifCommandAsync(user, $"**{Context.Message.Author.Username}** обнял **{user.Username}**", "anime hug");
+            await RunGifCommandAsync(user, $"**{Context.Message.Author.Username}** обнял **{user.Username}**", "anime hug");
         }
 
         [Command("poke")]
-        public async Task Poke(SocketGuildUser user = null!, EmbedCommand embedCommand = null!)
+        public async Task Poke(SocketGuildUser user = null!)
         {
-            await embedCommand.RunGifCommandAsync(user, $"**{Context.Message.Author.Username}** тыкнул в **{user.Username}**", "anime poke");
+            await RunGifCommandAsync(user, $"**{Context.Message.Author.Username}** тыкнул в **{user.Username}**", "anime poke");
         }
 
         [Command("cuddle")]
-        public async Task Cuddle(SocketGuildUser user = null!, EmbedCommand embedCommand = null!)
+        public async Task Cuddle(SocketGuildUser user = null!)
         {
-            await embedCommand.RunGifCommandAsync(user, $"**{Context.Message.Author.Username}** прижимается к **{user.Username}**", "anime cuddle");
+            await RunGifCommandAsync(user, $"**{Context.Message.Author.Username}** прижимается к **{user.Username}**", "anime cuddle");
         }
 
         [Command("pat")]
-        public async Task Pat(SocketGuildUser user = null!, EmbedCommand embedCommand = null!)
+        public async Task Pat(SocketGuildUser user = null!)
         {
-            await embedCommand.RunGifCommandAsync(user, $"**{Context.Message.Author.Username}** погладил **{user.Username}**", "anime pat");
+            await RunGifCommandAsync(user, $"**{Context.Message.Author.Username}** погладил **{user.Username}**", "anime pat");
         }
 
         [Command("tickle")]
-        public async Task Tickle(SocketGuildUser user = null!, EmbedCommand embedCommand = null!)
+        public async Task Tickle(SocketGuildUser user = null!)
         {
-            await embedCommand.RunGifCommandAsync(user, $"**{Context.Message.Author.Username}** щекочет **{user.Username}**", "anime tickle");
+            await RunGifCommandAsync(user, $"**{Context.Message.Author.Username}** щекочет **{user.Username}**", "anime tickle");
         }
+
+       private async Task RunGifCommandAsync(SocketGuildUser user, string description, string gifSearch)
+       {
+            if (user == null)
+            {
+                var embed = new VrotEmbedBuilder()
+                    .WithDescription("**Укажите пользователя**")
+                    .WithCurrentTimestamp()
+                    .Build();
+                await ReplyAsync(embed: embed);
+            }
+            else
+            {
+                var gifUrl = await _tenorService.GetRandomGifUrlAsync(gifSearch);
+
+                var embed = new VrotEmbedBuilder()
+                    .WithDescription(description)
+                    .WithImageUrl(gifUrl)
+                    .Build();
+                await ReplyAsync(embed: embed);
+            }
+       }
     }
 }
